c04a3f5c79235a14f36c1a5073f726e84a018f00
hasan9218 <hasanbhuiyan503@gmail.com>
2025-11-15 22:44:41 +0600

    update page added

--- src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-const Spinner = () => {
-  return (
-    <div className="flex justify-center items-center py-20">
-      <div className="h-12 w-12 border-4 border-green-600 border-t-transparent rounded-full animate-spin"></div>
-    </div>
-  );
-};
-
-export default Spinner;
